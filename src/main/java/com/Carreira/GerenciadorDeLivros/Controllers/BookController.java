1b0539d6079d8e0ac54c07bc529ff5684b643a56
Miguel Carreira <miguelitocarreira@hotmail.com>
2026-03-16 20:14:21 -0300

    Controller de Book

---
+++ src/main/java/com/Carreira/GerenciadorDeLivros/Controllers/BookController.java
@@ -0,0 +1,10 @@
+package com.Carreira.GerenciadorDeLivros.Controllers;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequestMapping(path = "/books")
+public class BookController {
+
+}
