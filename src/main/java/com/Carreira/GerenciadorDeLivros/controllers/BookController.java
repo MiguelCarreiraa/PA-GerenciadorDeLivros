01d71c11933f62c357ddd8c4f116e3821b6a3157
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-17 15:05:33 -0300

    CRUD COMPLETO

--- src/main/java/com/Carreira/GerenciadorDeLivros/controllers/BookController.java
+++ src/main/java/com/Carreira/GerenciadorDeLivros/controllers/BookController.java
@@ -9,8 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
+@RestController
 @RequestMapping(path = "/books")
 public class BookController {
 
@@ -25,12 +26,38 @@ public class BookController {
 
     @PostMapping
     public ResponseEntity<BookModel> criarBook(@RequestBody BookModel bookModel){
-        return ResponseEntity.status(204).body(bookModel);
+        BookModel novo = bookService.criarBook(bookModel);
+        return ResponseEntity.status(201).body(novo);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletarBook (@PathVariable Long id){
         bookService.deletar(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookModel> buscarPorId(@PathVariable Long id){
+        Optional<BookModel> book = bookService.buscarPorId(id);
+
+        if(book.isPresent()){
+            return ResponseEntity.ok(book.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookModel> atualizar(
+            @PathVariable Long id,
+            @RequestBody BookModel bookModel){
+
+        BookModel atualizado = bookService.atualizar(id, bookModel);
+
+        if(atualizado == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(atualizado);
     }
 }
